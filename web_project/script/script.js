e271acc5a97fe90d674a2b6a37db51826edcf1db
Samuel Weistroffer <123250590+SamuelW05@users.noreply.github.com>
2024-05-23 10:34:27 +0200

    Update script.js

--- web_project/script/script.js
+++ web_project/script/script.js
@@ -11,8 +11,16 @@ function quizConfirm() {
 
 function quizSubmit() {
     if (quizConfirm()) {
-        alert("Thank you for your submission!");
-    }
-    else
+        var input = ["monument", "museum", "name", "email", "phone", "date1", "date2"];
+        var confirmationMessage = "Confirmation Order:\n\n";
+        
+        for (var i = 0; i < input.length; i++) {
+            var x = document.forms[0][input[i]].value;
+            confirmationMessage += input[i] + ": " + x + "<br>";
+        }
+        
+        window.open("", "_blank").document.write("<h1>Confirmation Order</h1><p>" + confirmationMessage + "</p>");
+    } else {
         alert("Please fill in all the fields!");
+    }
 }
